5f6bb36262884dc9e18a5d952c9f768ecf3cd27b
jawad727 <jawaddhussein@gmail.com>
2019-11-04 18:38:12 -0800

    createpost working with dynamoDB, also created other CRUD methods in serverless file

--- handler.js
+++ handler.js
@@ -1,18 +1,33 @@
 'use strict';
+const AWS = require("aws-sdk");
+const db = new AWS.DynamoDB.DocumentClient({ apiVersion: "2012-08-10" })
+const uuid = require("uuid/v4")
 
-module.exports.hello = async event => {
+const postsTable = process.env.POSTS_TABLE;
+
+function response(statusCode, message) {
   return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+    statusCode: statusCode,
+    body: JSON.stringify(message)
+  }
+}
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
+module.exports.createPost = (event, context, callback) => {
+  const reqBody = JSON.parse(event.body)
+
+  const post = {
+    id: uuid(),
+    createdAt: new Date().toISOString(),
+    userId: 1,
+    title: reqBody.title,
+    body: reqBody.body
+  }
+
+  return db.put({
+    TableName: postsTable,
+    Item: post
+  }).promise().then(() => {
+    callback(null, response(201, post))
+  })
+  .catch(err => response(null, response(err.statusCode, err)));
+}
