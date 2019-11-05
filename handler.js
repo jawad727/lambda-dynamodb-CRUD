9cc52bdf1864f3a63b18cf7bdd9dbeb0e0041187
jawad727 <jawaddhussein@gmail.com>
2019-11-04 19:20:49 -0800

    all crud functions complete but not tested

--- handler.js
+++ handler.js
@@ -22,6 +22,10 @@ function sortByDate(a, b) {
 module.exports.createPost = (event, context, callback) => {
   const reqBody = JSON.parse(event.body)
 
+  if (!reqBody.title || reqBody.title.trim() === "" || !reqBody.body || reqBody.body.trim() === "") {
+    return callback(nuull, response(400, { err: "Post must have a title and body and they must not be empty" }))
+  }
+
   const post = {
     id: uuid(),
     createdAt: new Date().toISOString(),
@@ -45,4 +49,74 @@ module.exports.getAllPosts = (event, context, callback) => {
   }).promise().then(res => {
     callback(null, response(200, res.Items.sort(sortByDate)))
   }).catch(err => callback(null, response(err.statusCode, err)))
+}
+// Get number of posts
+module.exports.getPosts = (event, context, callback) => {
+  const numberOfPosts = event.pathParameters.number;
+  const params = {
+    TableName: postsTable,
+    Limit: numberOfPosts
+  }
+  return db.scan(params)
+  .promise()
+  .then(res => {
+    callback(null, response(200, res.Items.sort(sortByDate)))
+  }).catch(err => callback(null, response(err.statusCode, err)))
+}
+// Get a single post
+module.exports.getPost = (event, context, callback) => {
+  const id = event.pathParameters.id;
+  const params = {
+    Key:{
+      id: id
+    },
+    TableName: postsTable
+  }
+  return db.get(params).promise()
+  .then(res => {
+    if (res.Item) { callback(null, resonse(200, res.Item)) }
+    else { callback(null, response(404, {error: "Post not found"})) }
+  })
+  .catch(err => callback(null, response(err.statusCode, err)))
+}
+// Update a post
+module.exports.updatePost = (event, context, callback) => {
+  const id = event.pathParameters.id;
+  const body = JSON.parse(event.body);
+  const paramName = body.paramName;
+  const paramValue = body.paramValue;
+
+  const params = {
+    Key: {
+      id: id
+    },
+    TableName: postsTable,
+    ConditionExpression: "attribute_exists(id)",
+    UpdateExpression: "set " + paramName + " = :v",
+    ExpressionAttributeValues = {
+      ":v": paramValue 
+    },
+    ReturnValue: "ALL_NEW"
+  }
+
+  return db.update(params)
+  .promise()
+  .then(res => {
+    callback(null, response(200, res))
+  })
+  .catch(err => callback(null, resonse(err.statusCode, err)))
+}
+// Delete a post
+module.exports.deletePost = (event, context, callback) => {
+  const id = event.pathParameters.id;
+  const params = {
+    Key: {
+      id: id
+    },
+    TableName: postsTable
+  }
+  return db.delete(params)
+  .promise()
+  .then(() => callback(null, response(200, { message: "post deleted successfully" })))
+  .catch(err => callback(null, response(err.statusCode, err)))
 }
